eb7a84450b9e436169b98e2fa7d54263af689ff2
Ryan Castelli <ryancastelli19@gmail.com>
2024-09-23 15:28:47 -0300

    feat: add jokes page with CSS styling

---
+++ Semana-06-atividade-05/compass-piadas/src/components/Joke/Joke.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/prop-types */
+import { useState } from 'react';
+import './Joke.css';
+
+function Joke({ jokeItem, removeJoke }) {
+  const [showAnswer, setShowAnswer] = useState(false);
+
+  return (
+    <div className="joke">
+      <p>{jokeItem.joke}</p>
+      {showAnswer && <p className="response">{jokeItem.response}</p>}
+      <button onClick={() => setShowAnswer(!showAnswer)}>
+        {showAnswer ? 'Esconder resposta' : 'Ver resposta'}
+      </button>
+      <button onClick={() => removeJoke(jokeItem.id)} className="delete-btn"> Delete</button>
+    </div>
+  );
+}
+
+export default Joke;
+
