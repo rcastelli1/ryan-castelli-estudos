3c50f92bb3bfe41fccda7fff3d0a561dd4038f12
Ryan Castelli <ryancastelli19@gmail.com>
2024-09-30 17:05:26 -0300

    feat(Jokes): add buttons to toggle answer visibility and delete joke

--- Semana-07-atividade-06/compass-piadas/src/components/Joke/Joke.jsx
+++ Semana-07-atividade-06/compass-piadas/src/components/Joke/Joke.jsx
@@ -5,7 +5,14 @@ function Joke({ jokeItem, removeJoke }) {
   const [showAnswer, setShowAnswer] = useState(false)
 
   return (
-    <div className="joke">Joke</div>
+    <div className="joke">
+      <p>{jokeItem.joke}</p>
+      {showAnswer && <p className="response">{jokeItem.response}</p>}
+      <button onClick={() => setShowAnswer(!showAnswer)}>
+        {showAnswer ? 'Esconder resposta' : 'Ver resposta'}
+      </button>
+      <button onClick={() => removeJoke(jokeItem.id)} className="delete-btn">Delete</button>
+    </div>
     
   );
 }
